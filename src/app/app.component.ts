c22190995c56ef1d47a1c052fddf5108f0333461
leitoj <barriosjl2003@gmail.com>
2021-12-01 04:14:33 -0500

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-app';
+  ngOnInit(){
+    AOS.init();
+  }
 }
+
+
+
